5ab1498590483b009144d8036c7ebaf610357935
Ting, Li-Jung <lting@my.centennialcollege.ca>
2020-10-30 08:29:17 -0400

    connect to MongoDB Altas sucess

--- server/config/db.js
+++ server/config/db.js
@@ -2,6 +2,6 @@ module.exports = {
   //local MongoDB deployment ->
   //"URI": "mongodb://localhost/book_store"
 
-  //try to put database on cloud
+  //try to put database on cloud (sucessful)
   "URI": "mongodb+srv://lting:lting123@cluster0.fo6g2.mongodb.net/book_store?retryWrites=true&w=majority"
 };
